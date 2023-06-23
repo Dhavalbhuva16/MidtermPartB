805d7a99d41c8ab228762d9e7dad1f80245f89b4
Dhaval Bhuva <bhuvadh@sheridancollege.ca>
2023-06-23 13:10:22 -0400

    answer 3

---
+++ src/TestUserProfile.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import midtermexam_versiona_extensioncode.UserProfile;
+// part of answer 3
+public class TestUserProfile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Display available genres
+        String[] genres = {"Comedy", "Drama", "Action", "Mystery"};
+        System.out.println("Available Genres:");
+        for (String genre : genres) {
+            System.out.println("- " + genre);
+        }
+
+        // Prompt user for name
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        // Prompt user for favorite genre
+        System.out.print("Choose your favorite genre: ");
+        String favoriteGenre = input.nextLine();
+
+        // Create user profile
+        UserProfile userProfile = new UserProfile(name, favoriteGenre);
+
+        // Display confirmation message
+        System.out.println("Your user profile has been created!");
+    }
+}
